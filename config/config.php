dfd57201d51748833154953404681b8f2368bc04
Yaşar Kaan HELVA <yaarkaan@gmail.com>
2022-01-21 20:29:18 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,13 +1,16 @@
 <?php
 
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-
-$host = "localhost"; /* Host name */
-$user = "root"; /* User */
-$password = ""; /* Password */
-$dbname = "project_database"; /* Database name */
-
-$con = mysqli_connect($host, $user, $password,$dbname);
 
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
